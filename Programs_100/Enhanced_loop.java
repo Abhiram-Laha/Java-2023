10a0b182ba016d4de0c7d56cd1453e858b2a0778
Abhiram Laha <abhiramlaha2020@gmail.com>
2023-08-02 10:05:50 +0530

    fg

---
+++ Programs_100/Enhanced_loop.java
@@ -0,0 +1,16 @@
+public class Enhanced_loop {
+    public static void main(String[] args) {
+        int arr[] = {5,9,3,4,5,6,7,9,2,1,3,1,4,4,4,4,7,7,8,57,8,76,74,357,93,7,567,358,7,687,5687,5,745,758,7,587,58};
+
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+
+        String str[] = {"hjdsgf","dgfe","gfyuswgf","hewryuwofih","bhfjysgf","hruiyweiyha","urhwuireguw"};
+
+        for(String s:str){
+            System.out.println(s);
+        }
+    }
+    
+}

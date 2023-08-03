2850862de9f28422a78a1e6bb9f27d3bd2a75672
Abhiram Laha <abhiramlaha2020@gmail.com>
2023-08-03 22:56:26 +0530

    add

---
+++ Loops/Table.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class Table {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for(int i=1; i<=10; i++){
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+}
